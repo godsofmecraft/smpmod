edfc9d634df5929459285b8e2ce5bce2d45b4248
godsofmecraft <godsofmecraft@gmail.com>
2024-09-10 12:36:51 +0100

    datagen added updated

--- src/main/java/me/godsofmecraft/smpmod/SmpmodDataGenerator.java
+++ src/main/java/me/godsofmecraft/smpmod/SmpmodDataGenerator.java
@@ -1,11 +1,20 @@
 package me.godsofmecraft.smpmod;
 
+import me.godsofmecraft.smpmod.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class SmpmodDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
+
 
 	}
 }
