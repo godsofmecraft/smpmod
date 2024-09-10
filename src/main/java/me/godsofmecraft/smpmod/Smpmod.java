d917837976661a2b9ff30d81ec205a7ba2186995
godsofmecraft <godsofmecraft@gmail.com>
2024-09-10 10:12:20 +0100

    Initial commit

---
+++ src/main/java/me/godsofmecraft/smpmod/Smpmod.java
@@ -0,0 +1,16 @@
+package me.godsofmecraft.smpmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Smpmod implements ModInitializer {
+	public static final String MOD_ID = "smpmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/me/godsofmecraft/smpmod/SmpmodClient.java
@@ -0,0 +1,10 @@
+package me.godsofmecraft.smpmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmpmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
